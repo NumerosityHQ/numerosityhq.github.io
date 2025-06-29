df873b838c5cc071513f637c2d53bbb0897320d2
Pranav Somalinga <pranav.pyari@gmail.com>
2025-06-28 20:46:37 -0400

    Updated Landing Page

---
+++ script.js
@@ -0,0 +1,16 @@
+// Theme Toggle
+const toggle = document.getElementById('theme-toggle');
+toggle.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+});
+
+// Blob subtle movement on mouse move
+const blobs = document.querySelectorAll('.blob');
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX / window.innerWidth - 0.5;
+  const y = e.clientY / window.innerHeight - 0.5;
+  blobs.forEach((blob, i) => {
+    const factor = (i + 1) * 10;
+    blob.style.transform = `translate(${x * factor}px, ${y * factor}px)`;
+  });
+});
